265c843af71c73b52eb28d59abec3be72d33de76
Grzegorz <stysiok@o2.pl>
2017-03-22 14:50:10 +0100

    Template code
    Template code to start working on a project
    Przykładowy kod do rozpoczęcia pracy nad systemem

---
+++ Szybkapracapl/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Szybkapracapl.MvcApplication" Language="C#" %>
---
+++ Szybkapracapl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Szybkapracapl.Startup))]
+namespace Szybkapracapl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
