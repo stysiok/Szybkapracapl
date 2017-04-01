bff754a5af310474ce9f907e53c1ffe1a53f9540
Grzegorz <stysiok@o2.pl>
2017-04-01 13:24:44 +0200

    Create Offer
    Basic "Create Offer" form and OfferFromViewModel

---
+++ Szybkapracapl/Controllers/OffersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Szybkapracapl.Controllers
+{
+    public class OffersController : Controller
+    {
+        // GET: Offers
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Szybkapracapl/ViewModels/OfferFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Szybkapracapl.Models;
+
+namespace Szybkapracapl.ViewModels
+{
+    public class OfferFormViewModel
+    {
+        public string Name { get; set; }
+
+        public string Date { get; set; }
+        public string Time { get; set; }
+
+        public double Sallary { get; set; }
+
+        public string City { get; set; }
+        
+        public string Description { get; set; }
+    }
+
+}
